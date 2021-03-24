2438f6fc94907fb254c36bd6ceb4d4b6173f7410
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-03-24 11:39:24 +0530

    XAMARIN-34584 - Sample added

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/TreeViewBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class TreeViewBehavior : Behavior<SfTreeView>
+    {
+        #region Overrides
+        protected override void OnAttachedTo(SfTreeView bindable)
+        {
+            bindable.ItemHolding += Bindable_ItemHolding;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            bindable.ItemHolding -= Bindable_ItemHolding;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+
+        private void Bindable_ItemHolding(object sender, ItemHoldingEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("TreeView ItemHold", (e.Node.Content as FileManager).ItemName, "Close");
+        }
+
+        #endregion
+    }
+}
